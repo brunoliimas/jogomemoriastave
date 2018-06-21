8ce729b5e1e0c4dbe4442ea57cdefb47fb5f6fe3
Bruno Lima <ibrunoliimas@gmail.com>
2018-06-21 11:51:17 -0300

    Ajustar jogo do renan para não dar erro novamente

---
+++ js/script.js
@@ -0,0 +1,57 @@
+const personagens = ["wilson", "willow", "wolfgang", "wx78", "woodie", "wendy", "wes", "maxwell"];
+
+const imagens = embaralhar(duplicar(personagens));
+
+const cartas = document.querySelectorAll(".item");
+
+let jogando = false;
+
+function desativar(carta){
+    carta.classList.remove("ativo");
+    carta.style.backgroundImage = "url('imgs/logo.png')"
+    carta.onclick = clicar;
+}
+
+function ativar(carta){
+    carta.classList.add("ativo");
+    carta.style.backgroundImage = `url('imgs/${imagens[Number(carta.id)]}.png')`;
+    carta.onclick = null;
+}
+
+function verificar(){
+    let ativos = document.querySelectorAll(".ativo");
+    if(ativos[0].style.backgroundImage === ativos[1].style.backgroundImage){
+        for(let carta of ativos){
+            carta.classList.remove("ativo");
+        }
+    }
+    else{
+        for(let carta of ativos){
+            desativar(carta);
+        }
+    }
+}
+
+function clicar(){
+    
+    if(jogando){
+        jogando = false;
+    }
+    else{
+        jogando = true;
+    }
+    
+    if(this.classList.contains("ativo")){
+        desativar(this);
+    }
+    else{
+        ativar(this);
+    }
+    if(!jogando){
+        setTimeout(verificar, 1000);
+    }
+}
+
+for (let carta of cartas){
+    carta.onclick = clicar;
+}
